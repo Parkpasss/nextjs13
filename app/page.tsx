702043cf2b5afbbbd5aff951c81ee6684e12bd12
Park Jina <parkpass_@naver.com>
2023-05-17 12:41:57 +0900

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,15 @@
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+    <main className="container mx-auto">
+      <h1 className="mt-10 text-2xl">Home Page</h1>
+      <div>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus
+        aliquid repellendus, velit quod ad doloribus? Eius facere placeat sint
+        ipsa quod laudantium quis doloribus unde, ducimus atque dolores aut
+        temporibus.
+      </div>
+    </main>
+  )
+}
---
+++ app/project/page.tsx
@@ -0,0 +1,11 @@
+import Link from 'next/link'
+export default function Project() {
+  return (
+    <div className="container mx-auto">
+      <h1 className="mt-10 text-2xl font-bold">Project</h1>
+      <div>
+        <p>프로젝트 내용</p>
+      </div>
+    </div>
+  )
+}
